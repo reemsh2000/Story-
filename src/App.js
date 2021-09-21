7e4698653f67c9288042bb30eb7285926dd77427
reemsh2000 <sh951652@gmail.com>
2021-09-22 00:58:57 +0300

    edit in style

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import './App.css';
-import Welcome from './Components/Welcome';
-import Home from './Components/Home'
-import Characters from './Components/Characters'
-import Story from './Components/Story'
-import {BrowserRouter as Router,Switch, Route } from 'react-router-dom'
+import "./App.css";
+import Home from "./Components/Home";
+import Welcome from "./Components/Welcome";
+import Characters from "./Components/Characters";
+import Story from "./Components/Story";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-    <Router>
-           <Switch>
-           <Route exact path="/">
-            <Welcome/>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Welcome />
           </Route>
-         <Route exact path="/home">
-            <Home/>
-          </Route>   
-         <Route exact path="/characters">
-           <Characters/>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route exact path="/characters">
+            <Characters />
           </Route>
           <Route exact path="/story">
-            <Story/>
-          </Route> 
+            <Story />
+          </Route>
         </Switch>
-        </Router>
+      </Router>
     </div>
   );
 }
--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -1,32 +1,13 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import '../css/style.css';
+import React from 'react';
 function Welcome() {
-  const handleInput=(e)=>{
-    localStorage.setItem('Name', JSON.stringify({name:e.target.value}))
-  }
-  return (
-  <div className='welcome-conatiner'>
-      <div className='main'>
-         <div className='left'>
-         <h1> FOREST STORIES </h1>
-          <p> When grace is lost from life,come with a burst of song.</p>
-          <input type='text' placeholder='Enter your Name Please ^_^' onChange={(e)=>handleInput(e)}/>
-          <Link to="/home">Lets Start</Link>
-         </div> 
-        <img className='main-img'src='https://media.discordapp.net/attachments/857145728917045328/889969275069149264/2_1-01.png?width=618&height=458'alt='img'/>
 
-      </div>
-      <footer>
-        <img className='part' src='https://i.pinimg.com/564x/00/c1/db/00c1db5f9ce2e41bf9ee8e4974152fdb.jpg'alt='img'/>
-        <div className='description'>
-      <h4>   COMPANION </h4>
-     <p> In a small garden, with an old tree, children like to play under this tree...</p>
-     </div>
-</footer>
-</div>
 
-  )
+    return (
+        <div>
+            <h1>Hello home</h1>
+              
+        </div>
+    )
 }
 
 export default Welcome
