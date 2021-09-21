734d315c96acad1d8c5e82afad0edcb0503f2af0
reemsh2000 <sh951652@gmail.com>
2021-09-21 20:53:29 +0300

    create nav bar and routes Relates #8

---
+++ src/Components/Story.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Story() {
+    return (
+        <div>
+            <h2>Hello story</h2>
+        </div>
+    )
+}
+
+export default Story
---
+++ src/Components/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Welcome() {
+  return (
+    <div>
+     <h2> Hello Welcome</h2>
+    </div>
+  )
+}
+
+export default Welcome
