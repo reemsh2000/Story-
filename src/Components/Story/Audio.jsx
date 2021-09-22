2a2313c69293c6fce5ee0b2ac0825e8a8cdd86c3
reemsh2000 <sh951652@gmail.com>
2021-09-22 20:22:56 +0300

    add audio component

---
+++ src/Components/Story/Audio.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactAudioPlayer from 'react-audio-player'
+
+function Audio() {
+  return (
+    <ReactAudioPlayer
+    src="https://cdn1.esm3.com/music/156/khlebalakmen3aklak.mp3"
+      autoPlay
+      controls
+    />
+  )
+}
+
+export default Audio
