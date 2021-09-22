c713bf8b2f131edc71f18f9cd5820f4d5b607aa6
reemsh2000 <sh951652@gmail.com>
2021-09-22 16:30:46 +0300

    fix error

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,6 +1,6 @@
-const fetchData=()=>{
- return fetch('https://finalspaceapi.com/api/v0/character')
-        .then(res=>res.json())
-        .catch(console.error())
-}
+const fetchData = () => {
+  return fetch("https://finalspaceapi.com/api/v0/character")
+    .then((res) => res.json())
+    .catch(console.error);
+};
 export default fetchData;
