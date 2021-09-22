f56cde9d2c2bea8fdb57677a2e88ff23f094f2ae
reemsh2000 <sh951652@gmail.com>
2021-09-22 11:23:18 +0300

    add the id for just one time

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,30 +1,21 @@
-import React,{useState} from "react";
+import React from "react";
 
 function Button(props) {
   const { btnId } = props;
-  let dataArray= [];
-  const [IsUniqe, setIsUniqe] = useState(false)
-  const checkElement = (id, oldData) => {
-    oldData.forEach((ele) => {
-      if (ele.id === id) {
-         setIsUniqe(false);
-      }
-     else  setIsUniqe(true);
-    });
+  let dataArray = [];
+  //  Check Element if exsit , if not :add it to local storage
+  const AddElement = (id, oldData) => {
+    let allData = oldData.filter((ele) => ele.id !== id);
+    allData.push({ id: id });
+    localStorage.setItem("data", JSON.stringify(allData));
   };
-
+  // add id to local storage
   const addToSorage = (btnId) => {
     let oldData = JSON.parse(localStorage.getItem("data"));
     if (oldData !== null) {
-      checkElement(btnId, oldData)
-      console.log(oldData);
-      console.log('IsUniqe',IsUniqe)
-      if (IsUniqe){
-        oldData.push({id:btnId});
-        localStorage.setItem("data", JSON.stringify(oldData));
-      }
+      AddElement(btnId, oldData);
     } else {
-      dataArray.push({id:btnId});
+      dataArray.push({ id: btnId });
       localStorage.setItem("data", JSON.stringify(dataArray));
     }
   };
