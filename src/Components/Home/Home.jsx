e0f7b2e1d8f8c07af219e3049c6a5228a423475c
Reem Shamia <71079908+reemsh2000@users.noreply.github.com>
2025-02-27 23:45:11 +0200

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -19,7 +19,7 @@ function Home() {
           </div>
           <img
             className="main-img"
-            src="https://static.vecteezy.com/system/resources/previews/004/795/349/non_2x/hand-drawn-image-of-buck-with-large-antlers-cute-deer-illustration-animal-isolated-on-white-background-for-hunting-products-billboards-website-wildlife-cartoon-hand-drawing-vector.jpg"
+            src="../../utils/utils/WhatsApp Image 2025-02-27 at 23.42.29.jpeg"
             alt="img"
           />
         </div>
